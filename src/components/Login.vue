2ab33f645890ecdf1268c3ba6c9f91215ecfa0ef
NY Jan <nnyjan02426@gmail.com>
2025-09-29 23:31:37 +0800

    create inputs

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+function onUsernameInput(e) {
+  username.value = e.target.value
+}
+function onPasswordInput(e) {
+  password.value = e.target.value
+}
+</script>
+
+<template>
+  <h2>Sign Up</h2>
+  <form>
+    <label for="username">Username: </label> <br>
+    <input type="text" id="username" v-model="username">
+    <br>
+    <label for="password">Password: </label> <br>
+    <input type="text" id="password" v-model="password">
+    <br>
+    <button>Login</button>
+  </form>
+
+  <p>{{ username }} {{ password }}</p>
+</template>
+
+<style scoped></style>
+
